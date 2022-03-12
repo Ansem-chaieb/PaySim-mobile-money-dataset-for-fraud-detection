e9cdcc18007c5caf31a9107aaff58904f4996c53
Ansem-Chaieb <chaiebansem@gmail.com>
2022-03-12 13:56:23 +0000

    update streamlit

--- main.py
+++ main.py
@@ -1,22 +1,47 @@
+import os
 import gc
 import pandas as pd
+from sympy import im
 import yaml
+import argparse
 from box import Box
 
 from src.data.data_loading import data_load, memory_manage
 from src.data.data_processing import feature_engineering
+from src.data.EDA import launch_streamlit
 
-with open("src/config.yaml", "r") as ymlfile:
+with open("config.yaml", "r") as ymlfile:
     config = Box(yaml.safe_load(ymlfile))
 
-data = data_load(config.DATA_PATH)
-memory_manage(
-    data,
-    str_list=["nameDest", "type"],
-    int_list=["isFlaggedFraud", "isFraud", "step"],
-)
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--process",
+        help="Get informtions about your dataset.",
+        action="store_true",
+        default=False,
+    )
+    parser.add_argument(
+        "--eda",
+        help="Get informtions about your dataset.",
+        action="store_true",
+        default=False,
+    )
+    args = parser.parse_args()
+    return args
 
-gc.collect()
-feature_engineering(data, save=True, path=config.PROCESSED_DATA_PATH)
+args = get_args()
+if args.process:
+    data = data_load(config.DATA_PATH)
+    memory_manage(
+        data,
+        str_list=["nameDest", "type"],
+        int_list=["isFlaggedFraud", "isFraud", "step"],
+    )
 
-processed_data = data_load(config.PROCESSED_DATA_PATH)
+    gc.collect()
+    feature_engineering(data, save=True, path=config.PROCESSED_DATA_PATH)
+
+if args.eda:
+    os.system("streamlit run src/data/EDA.py")
+    
--- src/data/data_processing.py
+++ src/data/data_processing.py
@@ -1,3 +1,5 @@
+import pandas as pd
+
 def feature_engineering(data, save=False, path=None):
     assert (save == True) & (
         path == None
@@ -14,6 +16,125 @@ def feature_engineering(data, save=False, path=None):
 
     data.to_csv(path)
 
+def data_analysis(data):
+    trans_type = pd.DataFrame(
+        {
+            "isFraud": (data.groupby("type")["isFraud"].agg("sum") / 8213)
+            * 100,
+            "count": (data["type"].value_counts() / data.shape[0]) * 100,
+        }
+    ).reset_index(level=0)
+    trans_type.columns = trans_type.columns.str.replace("index", "type")
+
+    transaction_duration = pd.DataFrame(
+        {
+            "isFraud": (
+                data.groupby("transaction_duration")["isFraud"].agg("sum")
+                / 8213
+            )
+            * 100,
+            "count": (
+                data["transaction_duration"].value_counts() / data.shape[0]
+            )
+            * 100,
+        }
+    ).reset_index(level=0)
+    transaction_duration.columns = (
+        transaction_duration.columns.str.replace(
+            "index", "transaction_duration"
+        )
+    )
+
+    orig_transaction_error = pd.DataFrame(
+        {
+            "isFraud": (
+                data.groupby("orig_transaction_error")["isFraud"].agg(
+                    "sum"
+                )
+                / 8213
+            )
+            * 100,
+            "count": (
+                data["orig_transaction_error"].value_counts()
+                / data.shape[0]
+            )
+            * 100,
+        }
+    ).reset_index(level=0)
+    orig_transaction_error.columns = (
+        orig_transaction_error.columns.str.replace(
+            "index", "orig_transaction_error"
+        )
+    )
+
+    amount_oldbalanceOrg = pd.DataFrame(
+        {
+            "isFraud": (
+                data.groupby("amount_oldbalanceOrg")["isFraud"].agg("sum")
+                / 8213
+            )
+            * 100,
+            "count": (
+                data["amount_oldbalanceOrg"].value_counts() / data.shape[0]
+            )
+            * 100,
+        }
+    ).reset_index(level=0)
+    amount_oldbalanceOrg.columns = (
+        amount_oldbalanceOrg.columns.str.replace(
+            "index", "amount_oldbalanceOrg"
+        )
+    )
+
+    dest_transaction_error = pd.DataFrame(
+        {
+            "isFraud": (
+                data.groupby("dest_transaction_error")["isFraud"].agg(
+                    "sum"
+                )
+                / 8213
+            )
+            * 100,
+            "count": (
+                data["dest_transaction_error"].value_counts()
+                / data.shape[0]
+            )
+            * 100,
+        }
+    ).reset_index(level=0)
+    dest_transaction_error.columns = trans_type.columns.str.replace(
+        "index", "dest_transaction_error"
+    )
+
+    orig_dest = pd.DataFrame(
+        {
+            "isFraud": (
+                data.groupby("orig_dest")["isFraud"].agg("sum") / 8213
+            )
+            * 100,
+            "count": (data["orig_dest"].value_counts() / data.shape[0])
+            * 100,
+        }
+    ).reset_index(level=0)
+    orig_dest.columns = trans_type.columns.str.replace(
+        "index", "orig_dest"
+    )
+
+    flag = pd.DataFrame({'isFraud' : (data.groupby('isFlaggedFraud')['isFraud'].agg('sum') /8213) * 100,
+             'count': (data['isFlaggedFraud'].value_counts()/data.shape[0]) * 100}).reset_index(level=0)
+    flag.columns = flag.columns.str.replace(
+        "index", "flag"
+    )
+    
+    return (
+        trans_type,
+        transaction_duration,
+        orig_transaction_error,
+        amount_oldbalanceOrg,
+        dest_transaction_error,
+        orig_dest,
+        flag,
+    )
 
 def amount_oldbalanceOrg(row):
     if row["oldbalanceOrg"] - row["amount"] == 0:
